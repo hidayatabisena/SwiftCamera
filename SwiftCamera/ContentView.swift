b30faa796b1cd0669f3aa1358a4ede767bbc3b38
Han Sena <abisena@outlook.com>
2024-05-24 22:03:41 +0700

    Initial Commit

---
+++ SwiftCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCamera
+//
+//  Created by Hidayat Abisena on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCamera/SwiftCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCameraApp.swift
+//  SwiftCamera
+//
+//  Created by Hidayat Abisena on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
